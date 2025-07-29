b2fd9321ab34f921f37fee55cbb2a25a90ef0ec3
magicwandguy <iitdelhipakal@gmail.com>
2025-07-29 23:35:18 +0530

    spells.js

--- spells.js
+++ spells.js
@@ -1,74 +1,74 @@
 // spells.js
 const spellMap = {
   // ✨ Utility / Light Magic
-  "lumos": { color: "white" },
-  "nox": { color: "gray" },
-  "accio": { color: "lightblue" },
-  "alohomora": { color: "lightgreen" },
-  "reparo": { color: "lightgoldenrodyellow" },
-  "wingardium leviosa": { color: "peachpuff" },
-  "muffliato": { color: "lightcoral" },
-  "rictusempra": { color: "salmon" },
-  "sonorus": { color: "khaki" },
-  "quietus": { color: "thistle" },
-  "orchideous": { color: "orchid" },
-  "scourgify": { color: "mintcream" },
-  "tergeo": { color: "powderblue" },
-  "lumos maxima": { color: "whitesmoke" },
+  "lumos": { color: "#ffffff" }, // white
+  "nox": { color: "#888888" }, // gray
+  "accio": { color: "#3399ff" }, // bright blue
+  "alohomora": { color: "#00cc66" }, // green
+  "reparo": { color: "#ffcc33" }, // yellow-gold
+  "wingardium leviosa": { color: "#ff9999" }, // soft red
+  "muffliato": { color: "#ff6666" }, // red-pink
+  "rictusempra": { color: "#ff5050" }, // strong red
+  "sonorus": { color: "#ff9933" }, // orange
+  "quietus": { color: "#9966cc" }, // purple
+  "orchideous": { color: "#cc66cc" }, // violet
+  "scourgify": { color: "#00cccc" }, // teal
+  "tergeo": { color: "#66ccff" }, // light blue
+  "lumos maxima": { color: "#ffff99" }, // light yellow
 
   // 🛡️ Defensive Spells
-  "protego": { color: "gold" },
-  "expelliarmus": { color: "red" },
-  "expecto patronum": { color: "#66ccff" },
-  "finite incantatem": { color: "lavender" },
-  "episkey": { color: "palegreen" },
-  "protego maxima": { color: "goldenrod" },
-  "repello muggletum": { color: "beige" },
-  "salvio hexia": { color: "silver" },
-  "priori incantato": { color: "lightgray" },
+  "protego": { color: "#ffd700" }, // gold
+  "expelliarmus": { color: "#ff0000" }, // red
+  "expecto patronum": { color: "#66ccff" }, // icy blue
+  "finite incantatem": { color: "#ccccff" }, // light purple
+  "episkey": { color: "#66ff66" }, // green
+  "protego maxima": { color: "#ffaa00" }, // gold-orange
+  "repello muggletum": { color: "#cccc99" }, // sand
+  "salvio hexia": { color: "#cccccc" }, // silver
+  "priori incantato": { color: "#999999" }, // light gray
 
   // ⚔️ Offensive / Dueling Spells
-  "stupefy": { color: "blue" },
-  "petrificus totalus": { color: "slateblue" },
-  "incendio": { color: "orangered" },
-  "reducto": { color: "darkorange" },
-  "confringo": { color: "crimson" },
-  "expulso": { color: "mediumvioletred" },
-  "sectumsempra": { color: "maroon" },
-  "bombarda": { color: "firebrick" },
-  "bombarda maxima": { color: "darkgoldenrod" },
-  "deprimo": { color: "brown" },
+  "stupefy": { color: "#0033cc" }, // deep blue
+  "petrificus totalus": { color: "#6666cc" }, // slate
+  "incendio": { color: "#ff3300" }, // fire red
+  "reducto": { color: "#cc6600" }, // burnt orange
+  "confringo": { color: "#cc0000" }, // deep red
+  "expulso": { color: "#cc0066" }, // magenta
+  "sectumsempra": { color: "#800000" }, // maroon
+  "bombarda": { color: "#990000" }, // dark red
+  "bombarda maxima": { color: "#ff6600" }, // bright orange
+  "deprimo": { color: "#663300" }, // brown
 
   // 🔮 Miscellaneous / Neutral
-  "obliviate": { color: "lightsteelblue" },
-  "impervius": { color: "skyblue" },
-  "riddikulus": { color: "plum" },
-  "tarantallegra": { color: "orchid" },
-  "langlock": { color: "indianred" },
-  "colovaria": { color: "mediumaquamarine" },
-  "confundo": { color: "mediumpurple" },
-  "homenum revelio": { color: "ivory" },
-  "aparecium": { color: "linen" },
-  "point me": { color: "burlywood" },
+  "obliviate": { color: "#6699cc" }, // steel blue
+  "impervius": { color: "#00bfff" }, // sky blue
+  "riddikulus": { color: "#9966ff" }, // purple
+  "tarantallegra": { color: "#cc3399" }, // pink-violet
+  "langlock": { color: "#cc0000" }, // red
+  "colovaria": { color: "#33cc99" }, // turquoise
+  "confundo": { color: "#9999ff" }, // pale violet
+  "homenum revelio": { color: "#eeeeee" }, // off-white
+  "aparecium": { color: "#ffffcc" }, // parchment yellow
+  "point me": { color: "#ffcc99" }, // tan
 
   // ☠️ Dark Magic (Curses)
-  "crucio": { color: "darkred" },
-  "imperio": { color: "midnightblue" },
-  "avada kedavra": { color: "limegreen" },
-  "morsmordre": { color: "black" },
-  "fiendfyre": { color: "darkorange" },
-  "conjunctivitis": { color: "deeppink" },
-  "dolohov's curse": { color: "sienna" },
+  "crucio": { color: "#8b0000" }, // dark red
+  "imperio": { color: "#00008b" }, // dark blue
+  "avada kedavra": { color: "#00ff00" }, // neon green
+  "morsmordre": { color: "#111111" }, // black
+  "fiendfyre": { color: "#ff4500" }, // blazing orange
+  "conjunctivitis": { color: "#ff1493" }, // hot pink
+  "dolohov's curse": { color: "#a0522d" }, // sienna
 
   // 🧪 Transfiguration / Alchemy
-  "animagus": { color: "lightseagreen" },
-  "ferula": { color: "seashell" },
-  "engorgio": { color: "navajowhite" },
-  "reducio": { color: "wheat" },
-  "alecto's curse": { color: "indigo" },
-  "geminio": { color: "cornsilk" },
-  "multicorfors": { color: "lightcyan" },
+  "animagus": { color: "#20b2aa" }, // teal
+  "ferula": { color: "#f5f5dc" }, // beige
+  "engorgio": { color: "#ffcc66" }, // peach
+  "reducio": { color: "#deb887" }, // light brown
+  "alecto's curse": { color: "#4b0082" }, // indigo
+  "geminio": { color: "#ffffe0" }, // pale yellow
+  "multicorfors": { color: "#e0ffff" }, // pale cyan
 
   // 🪄 Add more spells below if needed
-  "ok": { color: "cyan" }
+  "ok": { color: "#00ffff" } // cyan
 };

3b9d881836de05e5fb82b94bc31bb4ae3212224e
magicwandguy <iitdelhipakal@gmail.com>
2025-07-29 23:29:32 +0530

    spells.js

--- spells.js
+++ spells.js
@@ -1,12 +1,74 @@
 // spells.js
 const spellMap = {
+  // ✨ Utility / Light Magic
   "lumos": { color: "white" },
-  "expelliarmus": { color: "red" },
-  "stupefy": { color: "blue" },
-  "avada kedavra": { color: "limegreen" },
-  "protego": { color: "gold" },
+  "nox": { color: "gray" },
+  "accio": { color: "lightblue" },
+  "alohomora": { color: "lightgreen" },
+  "reparo": { color: "lightgoldenrodyellow" },
+  "wingardium leviosa": { color: "peachpuff" },
+  "muffliato": { color: "lightcoral" },
+  "rictusempra": { color: "salmon" },
+  "sonorus": { color: "khaki" },
+  "quietus": { color: "thistle" },
+  "orchideous": { color: "orchid" },
+  "scourgify": { color: "mintcream" },
+  "tergeo": { color: "powderblue" },
+  "lumos maxima": { color: "whitesmoke" },
 
-  // 🪄 Add more spells below:
-  "ok": { color: "cyan" },
-  "expecto patronum": { color: "#66ccff" }
+  // 🛡️ Defensive Spells
+  "protego": { color: "gold" },
+  "expelliarmus": { color: "red" },
+  "expecto patronum": { color: "#66ccff" },
+  "finite incantatem": { color: "lavender" },
+  "episkey": { color: "palegreen" },
+  "protego maxima": { color: "goldenrod" },
+  "repello muggletum": { color: "beige" },
+  "salvio hexia": { color: "silver" },
+  "priori incantato": { color: "lightgray" },
+
+  // ⚔️ Offensive / Dueling Spells
+  "stupefy": { color: "blue" },
+  "petrificus totalus": { color: "slateblue" },
+  "incendio": { color: "orangered" },
+  "reducto": { color: "darkorange" },
+  "confringo": { color: "crimson" },
+  "expulso": { color: "mediumvioletred" },
+  "sectumsempra": { color: "maroon" },
+  "bombarda": { color: "firebrick" },
+  "bombarda maxima": { color: "darkgoldenrod" },
+  "deprimo": { color: "brown" },
+
+  // 🔮 Miscellaneous / Neutral
+  "obliviate": { color: "lightsteelblue" },
+  "impervius": { color: "skyblue" },
+  "riddikulus": { color: "plum" },
+  "tarantallegra": { color: "orchid" },
+  "langlock": { color: "indianred" },
+  "colovaria": { color: "mediumaquamarine" },
+  "confundo": { color: "mediumpurple" },
+  "homenum revelio": { color: "ivory" },
+  "aparecium": { color: "linen" },
+  "point me": { color: "burlywood" },
+
+  // ☠️ Dark Magic (Curses)
+  "crucio": { color: "darkred" },
+  "imperio": { color: "midnightblue" },
+  "avada kedavra": { color: "limegreen" },
+  "morsmordre": { color: "black" },
+  "fiendfyre": { color: "darkorange" },
+  "conjunctivitis": { color: "deeppink" },
+  "dolohov's curse": { color: "sienna" },
+
+  // 🧪 Transfiguration / Alchemy
+  "animagus": { color: "lightseagreen" },
+  "ferula": { color: "seashell" },
+  "engorgio": { color: "navajowhite" },
+  "reducio": { color: "wheat" },
+  "alecto's curse": { color: "indigo" },
+  "geminio": { color: "cornsilk" },
+  "multicorfors": { color: "lightcyan" },
+
+  // 🪄 Add more spells below if needed
+  "ok": { color: "cyan" }
 };
